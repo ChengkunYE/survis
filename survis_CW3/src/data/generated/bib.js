9fccee654cfc508d3f713841daf7640cb43eef69
ChengkunYE <147529001+ChengkunYE@users.noreply.github.com>
2024-05-15 22:02:42 +0100

    Add files via upload

---
+++ survis_CW3/src/data/generated/bib.js
@@ -0,0 +1,108 @@
+﻿define({ entries : {
+    "abualola2022v2v": {
+        "author": "Abualola, Huda and Otrok, Hadi and Mizouni, Rabeb and Singh, Shakti",
+        "journal": "Vehicular Communications",
+        "keywords": "Electric vehicle, V2V charging, QoS-OLSR, Protocol, Internet of vehicles, Ad-hoc",
+        "pages": "100427",
+        "publisher": "Elsevier",
+        "title": "A V2V charging allocation protocol for electric vehicles in VANET",
+        "type": "article",
+        "volume": "33",
+        "year": "2022"
+    },
+    "aravindhan2021smart": {
+        "author": "Aravindhan, K and Sangeetha, SKB and Periyakaruppan, K and Manoj, E and Sivani, R and Ajithkumar, S",
+        "booktitle": "2021 7th International Conference on Advanced Computing and Communication Systems (ICACCS)",
+        "keywords": "Navigation,Computational modeling,Loading,Decision making,Vehicular ad hoc networks,Load management,Servers,Electric Vehicles,Intelligent V2V Charging,Charging Models,VANETs",
+        "organization": "IEEE",
+        "pages": "1588--1591",
+        "title": "Smart charging navigation for VANET based electric vehicles",
+        "type": "inproceedings",
+        "volume": "1",
+        "year": "2021"
+    },
+    "arikumar2023software": {
+        "author": "Arikumar, KS and Prathiba, Sahaya Beni and Moorthy, Rajalakshmi Shenbaga and Srivastava, Gautam and Gadekallu, Thippa Reddy",
+        "journal": "IEEE Transactions on Network Science and Engineering",
+        "keywords": "Batteries,Electric vehicle charging,Servers,Schedules,Real-time systems,Processor scheduling,Prediction algorithms,Software-defined networking,vehicular edge computing,electric vehicles,charge scheduling and management,future demand prediction",
+        "publisher": "IEEE",
+        "title": "Software Defined Networking Assisted Electric Vehicle Charging: Towards Smart Charge Scheduling and Management",
+        "type": "article",
+        "year": "2023"
+    },
+    "cao2018mobile": {
+        "author": "Cao, Yue and Song, Houbing and Kaiwartya, Omprakash and Zhou, Bingpeng and Zhuang, Yuan and Cao, Yang and Zhang, Xu",
+        "journal": "IEEE Communications Magazine",
+        "keywords": "Servers,Big Data,Cloud computing,Electric vehicle charging,Smart cities,Smart grids,Carbon dioxide,Air pollution",
+        "number": "3",
+        "pages": "150--156",
+        "publisher": "IEEE",
+        "title": "Mobile edge computing for big-data-enabled electric vehicle charging",
+        "type": "article",
+        "volume": "56",
+        "year": "2018"
+    },
+    "li2020efficient": {
+        "author": "Li, Guangyu and Gong, Chen and Zhao, Lin and Wu, Jinsong and Boukhatem, Lila",
+        "booktitle": "2020 IEEE International conference on big data and smart computing (BigComp)",
+        "keywords": "Roads,Navigation,Vehicular ad hoc networks,Routing,Servers,Smart grids,Learning (artificial intelligence)",
+        "organization": "IEEE",
+        "pages": "263--270",
+        "title": "An efficient reinforcement learning based charging data delivery scheme in VANET-enhanced smart grid",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "liu2019smart": {
+        "author": "Liu, Jiajia and Guo, Hongzhi and Xiong, Jingyu and Kato, Nei and Zhang, Jie and Zhang, Yanning",
+        "journal": "IEEE Journal on Selected Areas in Communications",
+        "keywords": "Electric vehicle charging,Processor scheduling,Edge computing,Dynamic scheduling,Batteries,Vehicle dynamics,Smart grid,electric vehicle,charging scheduling,vehicular edge computing,deep reinforcement learning",
+        "number": "1",
+        "pages": "217--228",
+        "publisher": "IEEE",
+        "title": "Smart and resilient EV charging in SDN-enhanced vehicular edge computing networks",
+        "type": "article",
+        "volume": "38",
+        "year": "2019"
+    },
+    "radenkovic2018cognitivecharge": {
+        "author": "Radenkovic, Milena and Walker, Adam",
+        "booktitle": "Proceedings of the 4th ACM MobiHoc Workshop on Experiences with the Design and Implementation of Smart Objects",
+        "keywords": "smart energy, VANETs, DTNs",
+        "pages": "1--9",
+        "title": "CognitiveCharge: disconnection tolerant adaptive collaborative and predictive vehicular charging",
+        "type": "inproceedings",
+        "year": "2018"
+    },
+    "radenkovic2020energy": {
+        "author": "Radenkovic, Milena and Huynh, Vu San Ha",
+        "booktitle": "2020 Fifth International Conference on Fog and Mobile Edge Computing (FMEC)",
+        "keywords": "Vehicle dynamics,Learning (artificial intelligence),Real-time systems,Power system dynamics,Heuristic algorithms,Energy states,Nonhomogeneous media,Vehicular Edge and Fog Networks,Smart Charging,Spatial-Temporal Dynamic Energy Supply and Demand",
+        "organization": "IEEE",
+        "pages": "5--12",
+        "title": "Energy-aware opportunistic charging and energy distribution for sustainable vehicular edge and fog networks",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "vaidya2020smart": {
+        "author": "Vaidya, Binod and Mouftah, Hussein T",
+        "journal": "IET Smart Cities",
+        "keywords": "power grids, scheduling, electric vehicles, electric vehicle charging, smart cities, resource allocation, smart electric vehicle charging management, smart cities, EV penetration, uncoordinated charging, electric power grid, EV charging network, charging facilities, effective reservation management, charging strategy, smart EV charging management system, urban mobility paradigms, fossil fuel, carbon emission reduction, scarce charging station resource allocation, efficient slot allocation, user discontent factor, estimated charging cost, estimated charging time, average waiting time, CS congestion impact, scheduling scheme, B8520 Transportation",
+        "number": "1",
+        "pages": "4--13",
+        "publisher": "Wiley Online Library",
+        "title": "Smart electric vehicle charging management for smart cities",
+        "type": "article",
+        "volume": "2",
+        "year": "2020"
+    },
+    "yan2020reinforcement": {
+        "author": "Yan, Li and Shen, Haiying and Kang, Liuwang and Zhao, Juanjuan and Xu, Chengzhong",
+        "booktitle": "2020 IEEE 17th International Conference on Mobile Ad Hoc and Sensor Systems (MASS)",
+        "keywords": "Wireless communication,Wireless sensor networks,Inductive charging,Dynamics,Reinforcement learning,Dynamic scheduling,Vehicle dynamics",
+        "organization": "IEEE",
+        "pages": "401--409",
+        "title": "Reinforcement learning based scheduling for cooperative EV-to-EV dynamic wireless charging",
+        "type": "inproceedings",
+        "year": "2020"
+    }
+}});
